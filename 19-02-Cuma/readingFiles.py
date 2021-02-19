a95fd7078ef7f667751cc8adf686805cd6d41d01
cihatdev <cihat02020202@gmail.com>
2021-02-19 23:57:59 +0300

    add new day and add new lecture in 19. day

--- 19-02-Cuma/readingFiles.py
+++ 19-02-Cuma/readingFiles.py
@@ -8,14 +8,23 @@
 #     content = file_content.read()
 #     print(content)
 
-
-employee_file = open("./deneme.txt", "r")
+# read file that "r" parametre
+employee_file = open("./employees.txt", "r")
 
 # print(employee_file.readable())
 # print(employee_file.readlines())
 
 for employee in employee_file.readlines():
     print(employee)
+
+employee_file = open("./employees.txt", "a")
+# "a" parametresi ile dosyaya ekleme yapabiliriz. İsminden de anlaşılabileceği gibi add'in a sıdır.
+# Ayrıca "w" vardır bu da herşeyin üzerine yazar yani öncekileri siler ve daha sonra yeni gelen contenti ekler.
+# Eğer dosya yoksa yeni bir dosya yaratır.
+employee_file = open("./employees2.txt", "w")
+
+
+employee_file.write("\nToby - Human Resources")
 
 
 employee_file.close()
