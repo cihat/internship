74f031946a4357c3346ed768b4bd671f6311d031
cihatdev <cihat02020202@gmail.com>
2021-03-13 01:10:48 +0300

    update 03-12-cuma

--- 03-12-Cuma/pagination/routes/shop.js
+++ 03-12-Cuma/pagination/routes/shop.js
@@ -1,28 +1,28 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const shopController = require('../controllers/shop');
-const isAuth = require('../middleware/is-auth');
+const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+router.get("/", shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
-router.get('/products/:productId', shopController.getProduct);
+router.get("/products/:productId", shopController.getProduct);
 
-router.get('/cart', isAuth, shopController.getCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post('/cart', isAuth, shopController.postCart);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
-router.post('/create-order', isAuth, shopController.postOrder);
+router.post("/create-order", isAuth, shopController.postOrder);
 
-router.get('/orders', isAuth, shopController.getOrders);
+router.get("/orders", isAuth, shopController.getOrders);
 
-router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+router.get("/orders/:orderId", isAuth, shopController.getInvoice);
 
 module.exports = router;
